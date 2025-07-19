e7a72e00fba5e22e8e0130801c1944868bfce289
Sarath-33 <withsarath.m@gmail.com>
2025-07-19 21:47:58 +0530

    Completed js-exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr, element) {
+//     const newArray = [];
+//     arr.forEach(item => {
+//         if (!element.includes(item)) {
+//             newArray.push(item)
+//         }
+//     });
+//     return newArray;
+// };
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
 };
+console.log(removeFromArray([1,2,3,4],3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
