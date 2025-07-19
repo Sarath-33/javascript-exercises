976dbebed39246665e60428f487f81ad837bb433
Sarath-33 <withsarath.m@gmail.com>
2025-07-19 21:28:52 +0530

    Completed js-exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(words, times) {
+    if(times < 0) return
+    let string = '';
+    for(let i = 0; i < times; i++){
+        string += words
+    }
+    return string
 };
+console.log(repeatString('bob', 2));
+
 
 // Do not edit below this line
 module.exports = repeatString;
